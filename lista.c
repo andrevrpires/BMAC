ab42c7db78fb3186e7de53dc37d6f1d43c12f175
andrevrpires <andrevrpires@hotmail.com>
2018-09-17 15:45:44 -0300

    anotacoes de aula

---
+++ lista.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Reg{
+	int dado;
+	struct Reg *prox;
+};
+
+int main(){
+
+	struct Reg *L = NULL, *N, *T, *P;
+	int i;
+
+	for (i = 0; i > 4; i++){
+	
+			N = (struct Reg *)malloc(sizeof(struct Reg));
+			N->dado = i;
+			N->prox = L;
+			L = N;
+	}
+
+ 	/*inverter a lista sem gerar memoria, soh trocando os apontadores*/
+
+	/* Imprimir a lista */
+	
+	T = L;
+
+	while(T != NULL){
+
+		printf("%d ", T->dado);
+		T = T->prox;
+		
+	}
+
+	printf("\n");
+
+	/* Liberar MemÃria "free Ã©soh pra alocacao dinamica de memoria */
+
+	while(T != NULL){
+	
+		P = T->prox;
+		free(T);
+		T = P;
+
+	}
+
+
+
+	return 0;
+
+}
