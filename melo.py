4e341ade4fd6fc5d8513178d60f94e895ff342e1
andrevrpires <andrevrpires@hotmail.com>
2018-09-28 13:43:14 -0300

    subindo código pra procurar em odt

---
+++ melo.py
@@ -0,0 +1,36 @@
+import glob, zipfile, re, pyoo
+
+def removeXMLMarkup(s, replace_with_space):
+    s = re.compile("<!--.*?-->", re.DOTALL).sub('', s)
+    repl = ''
+    if replace_with_space:
+        repl = ' '
+    s = re.compile("<[^>]*>", re.DOTALL).sub(repl, s)
+    return s
+
+desktop = pyoo.Desktop()
+planilha = desktop.create_spreadsheet()
+sheet = planilha.sheets[0]
+linha = 0
+
+arquivos = (glob.glob("*.odt"))
+
+for f in arquivos:
+    myfile = zipfile.ZipFile(f)
+    listoffiles = myfile.infolist()
+
+    for s in listoffiles:
+        if s.orig_filename == "content.xml":
+            content = str(myfile.read(s.orig_filename), 'utf-8')
+            content = removeXMLMarkup(content, replace_with_space=1)
+            for w in content.split():
+                if re.search("[0-9]{6}", w):
+                    print(w)                
+                    sheet[linha, 0].value = f
+                    sheet[linha, 1].value = w
+                    linha += 1
+                    
+
+planilha.save('example.ods')
+planilha.close()
+
