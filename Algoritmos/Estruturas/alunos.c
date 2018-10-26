b5ef40a7e23cefd22853bc1c0273b6c64b5b9c49
andrevrpires <andrevrpires@hotmail.com>
2018-10-26 18:55:48 -0300

    terminando listas ligadas

--- Algoritmos/Estruturas/alunos.c
+++ Algoritmos/Estruturas/alunos.c
@@ -1,65 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Aluno{
-	char nome[100];
-	int n_usp;
-	float cr;
-	char sexo;
+ char nome[100];
+ int n_usp;
+ float cr;
+ char sexo;
 };
 
 void ImprimeAluno(struct Aluno A){
-
-	printf("Nome: %s\n", A.nome);
-	printf("N_USP: %d\n", A.n_usp);
-	printf("CR: %.2f\n", A.cr);
-	printf("Sexo: %c\n\n", A.sexo);
-
+ printf("Nome: %s\n", A.nome);
+ printf("N_USP: %d\n", A.n_usp);
+ printf("CR: %.2f\n", A.cr);
+ printf("Sexo: %c\n\n", A.sexo);
 }
 
 int main(){
-	char arquivo[100];
-	FILE *fp;
-	int nalunos, i;
-	struct Aluno *V;
-	printf("Digite o nome do arquivo: ");
-	scanf("%s", arquivo);
+ char arquivo[100];
+ FILE *fp;
+ int nalunos, i;
+ struct Aluno *V;
 
-	printf("size: %d", (int)sizeof(struct Aluno));
+ /*Recebe o nome do Arquivo*/
+ printf("Digite o nome do arquivo: ");
+ scanf("%s", arquivo);
+ printf("\n");
 
-	fp = fopen(arquivo, "r");
-	fscanf(fp, "%d", &nalunos);
+ /*printf("size: %d", (int)sizeof(struct Aluno));*/
 
-	V = (struct Aluno *)malloc(sizeof(struct Aluno)*nalunos);
+ /*A primeira linha do arquivo contem o numero total de alunos*/
+ fp = fopen(arquivo, "r");
+ fscanf(fp, "%d", &nalunos);
 
-	for(i = 0; i < nalunos; i++)
+ /*Podemos entao alocar a memoria necessaria*/
+ V = (struct Aluno *)malloc(sizeof(struct Aluno)*nalunos);
 
-		fscanf(fp, " %[^,],", V[i].nome);
-		fscanf(fp, "%d,", &V[i].n_usp);	
-		fscanf(fp, "%f,", &V[i].cr);
-		fscanf(fp, " %c", &V[i].sexo);
-		
-	fclose(fp);
+ /*Carregar os dados no vetor*/
+ for(i = 0; i < nalunos; i++){
+  fscanf(fp, " %[^,],", V[i].nome);
+  fscanf(fp, "%d,", &V[i].n_usp);	
+  fscanf(fp, "%f,", &V[i].cr);
+  fscanf(fp, " %c", &V[i].sexo);
+ }
 
-	for(i = 0; i < nalunos; i++)
-		ImprimeAluno(V[i]);
+ /*Fechar o arquivo de entrada de dados*/
+ fclose(fp);
 
-	/*fp = fopen("saida.bin", "wb");
+ /*Printa os dados do vetor*/
+ for(i = 0; i < nalunos; i++)
+  ImprimeAluno(V[i]);
 
-	fwrite(V, sizeof(struct Aluno), nalunos, fp);
+ /*Salva no arquivo em binario e libera memoria*/
+ fp = fopen("saida.bin", "wb");
+ fwrite(V, sizeof(struct Aluno), nalunos, fp);
+ fclose(fp);
+ free(V);
 
-	fclose(fp);
+ /*Testa o arquivo binario*/
+ /*
+ V = (struct Aluno *)malloc(sizeof(struct Aluno)*nalunos);
+ fp = fopen("saida.bin", "rb");
+ fread(V, sizeof(struct Aluno), nalunos, fp);
+ fclose(fp);
 
-	V = (struct Aluno *)malloc(sizeof(struct Aluno)*nalunos);
+ for(i = 0; i < nalunos; i++)
+  ImprimeAluno(V[i]);
 
-	fp = fopen("saida.bin", "rb");
+ free(V);
+ */
 
-	fread(V, sizeof(struct Aluno), nalunos, fp);
-
-	fclose(fp);*/
-
-	free(V);
-
-	return 0;
+ return 0;
 }
--- Algoritmos/Estruturas/lista.c
+++ Algoritmos/Estruturas/lista.c
@@ -1,37 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Reg{
+struct Bloco{
  int dado;
- struct Reg *prox;
+ struct Bloco *prox;
 };
 
+struct Bloco *AlocaNoLista(){
+ struct Bloco *q;
+ q = (struct Bloco *)malloc(sizeof(struct Bloco)); 
+ if(q == NULL)
+  exit(1);
+ return q;
+}
+
+void InsereAposListaLSskbca(struct Bloco **p, int x){
+ struct Bloco *n, *q;
+ q = *p
+ n = AlocaNoLista();
+ n->dado = x;
+ n->prox = q;
+ *p = n;
+}
+
+void RemoveAposListaLSskbca(struct Bloco **p){
+ struct Bloco *r;
+ r = *p;
+ if(r != NULL){
+  *p = r-prox;
+  free(r);
+ }
+}
+
+struct Bloco *CriaNoKbca(){
+ struct Bloco *q;
+ q = AlocaNoLista();
+ q->dado = -1;
+ q->prox = NULL;
+ return q;
+}
+
+void InsereAposLista(struct Bloco *p, int x){
+ struct Bloco *q;
+ q = AlocaNoLista();
+ q->dado = x;
+ q->prox = p->prox;
+ p->prox = q;
+}
+
+void RemoveAposLista(struct Bloco *p){
+ struct Bloco *q;
+ q = p->prox;
+ if(q != NULL){
+  p->prox = q->prox;
+  free(q);
+ }
+}
+
+void InsereAposCircular(struct Bloco *p, int x){
+
+}
+
+void RemoveAposCircular(struct Bloco *p){
+
+}
+
+void Imprimelista(struct Bloco *L){
+ struct Bloco *T;
+  T = L;
+  while(T != NULL){
+  printf("%d ", T->dado);
+  T = T->prox;
+ }
+ printf("\n");
+}
+
+void ImprimeCircular(struct Bloco *p){
+
+}
+
+void BuscaCircular(struct Bloco *p, int x)
+
+{}
+
+void Liberalista(struct Bloco *L){
+ struct Bloco *P;
+  while(L != NULL){
+  P = L->prox;
+  free(L);
+  L = P;
+ }
+}
+
+struct Bloco *Invertelista(struct Bloco *L){
+ struct Bloco *I, *A = NULL;
+ I = L;
+ while(I != NULL){
+  L = L->prox;
+  I->prox = A;
+  A = I;
+  I = L;
+ }
+ return A;
+}
+
 int main(){
- struct Reg *L = NULL, *N, *T, *P;
+ struct Bloco *L = NULL, *N;
  int i;
 
- for (i = 0; i > 4; i++){
-  N = (struct Reg *)malloc(sizeof(struct Reg));
+ for (i = 0; i < 4; i++){
+  N = (struct Bloco  *)malloc(sizeof(struct Bloco)); 
   N->dado = i;
   N->prox = L;
   L = N;
  }
 
- /*inverter a lista sem gerar memoria, soh trocando os apontadores*/
-
  /*Imprimir a lista*/
- T = L;
- while(T != NULL){
-  printf("%d ", T->dado);
-  T = T->prox;
- }
- printf("\n");
+ Imprimelista(L);
+ 
+ /*inverter a lista sem gerar memoria, soh trocando os apontadores*/
+ L = Invertelista(L);
 
+ /*Imprimir a lista invertida*/
+ Imprimelista(L);
+ 
  /*Liberar Memoria*/
- while(T != NULL){
-  P = T->prox;
-  free(T);
-  T = P;
- }
+ Liberalista(L);
+
  return 0;
 }
